90d010497920e281c312619df5ac2dc4e79a6236
Icarus-conf <brain.m.config@gmail.com>
2023-12-19 09:12:17 +0200

    updating

--- lib/morning_page.dart
+++ lib/morning_page.dart
@@ -142,111 +142,119 @@ class _SabahPageState extends State<SabahPage> {
           itemCount: texts.length,
           itemBuilder: (context, index) {
             counters.add(0);
+
             return Padding(
               padding: const EdgeInsets.symmetric(
                 vertical: 8,
               ),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Column(
-                    children: [
-                      AmiriText(
-                        text: texts[index],
-                        fontS: 25,
-                        textDirection: TextDirection.rtl,
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      AmiriText(
-                        text: subTexts[index],
-                        fontS: 14,
-                        textDirection: TextDirection.rtl,
-                        color: Colors.grey[600],
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      CircleAvatar(
-                        child: Text(
-                          countNumbers[index].toString(),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    counters[index]++;
+                  });
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Column(
+                      children: [
+                        AmiriText(
+                          text: texts[index],
+                          fontS: 25,
+                          textDirection: TextDirection.rtl,
                         ),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF086788),
-                              foregroundColor: Color(0xFF9CAFB7),
-                              shadowColor: Colors.grey[400],
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                  10.0,
-                                ),
-                              ),
-                              minimumSize: Size(100, 40),
-                            ),
-                            child: AmiriText(
-                              text: "إعادة",
-                              fontS: 18,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                if (counters[index] > 0) {
-                                  counters[index]--;
-                                }
-                              });
-                            },
+                        SizedBox(
+                          height: 8,
+                        ),
+                        AmiriText(
+                          text: subTexts[index],
+                          fontS: 14,
+                          textDirection: TextDirection.rtl,
+                          color: Colors.grey[600],
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        CircleAvatar(
+                          child: Text(
+                            countNumbers[index].toString(),
                           ),
-                          CircleAvatar(
-                            backgroundColor: Color(0xFF062726),
-                            radius: 25,
-                            child: Text(
-                              counters[index].toString(),
-                              style: TextStyle(
-                                fontSize: 18,
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xFF086788),
+                                foregroundColor: Color(0xFF9CAFB7),
+                                shadowColor: Colors.grey[400],
+                                elevation: 3,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    10.0,
+                                  ),
+                                ),
+                                minimumSize: Size(100, 40),
+                              ),
+                              child: AmiriText(
+                                text: "إعادة",
+                                fontS: 18,
                                 color: Colors.white,
-                                fontWeight: FontWeight.w700,
                               ),
+                              onPressed: () {
+                                setState(() {
+                                  if (counters[index] > 0) {
+                                    counters[index]--;
+                                  }
+                                });
+                              },
                             ),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
+                            CircleAvatar(
                               backgroundColor: Color(0xFF062726),
-                              foregroundColor: Color(0xFF66A182),
-                              shadowColor: Colors.grey[400],
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                  10.0,
+                              radius: 25,
+                              child: Text(
+                                counters[index].toString(),
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
                                 ),
                               ),
-                              minimumSize: Size(100, 40),
                             ),
-                            child: AmiriText(
-                              text: "تسبيح",
-                              fontS: 18,
-                              color: Colors.white,
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xFF062726),
+                                foregroundColor: Color(0xFF66A182),
+                                shadowColor: Colors.grey[400],
+                                elevation: 3,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    10.0,
+                                  ),
+                                ),
+                                minimumSize: Size(100, 40),
+                              ),
+                              child: AmiriText(
+                                text: "تسبيح",
+                                fontS: 18,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  counters[index]++;
+                                });
+                              },
                             ),
-                            onPressed: () {
-                              setState(() {
-                                counters[index]++;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
